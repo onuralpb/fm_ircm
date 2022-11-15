59f4022c39bdbe4245b6b2aeaee6bf6d6d650676
Onur Alp Baklaci <onuralpb@gmail.com>
2022-11-15 15:57:53 +0300

    General structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,80 @@
+import { useState, useEffect } from "react";
 import "./modules/libs";
+import Image from "./modules/images";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 function App() {
+  const [selectedPoint, setSelectedPoint] = useState(0);
+  const [voteStatus, setVoteStatus] = useState(false);
+  console.log("voteStatus: ", voteStatus);
+  const points = [1, 2, 3, 4, 5];
+
   return (
-    <Container className="pageContainer text-center">
-      <Row className="appLayout justify-content-center align-items-center">
+    <Container className="pageContainer">
+      <Row className="appLayout">
         <Col className="d-flex justify-content-center align-items-center">
-          <Row className="appContainer">
+          <Row className="appContainer shadow-lg">
             <Col>
-              <Row className="appContent justify-content-center row-cols-auto align-items-center">
-                <Col>1 of 3</Col>
-                <Col>Variable width content</Col>
-                <Col>3 of 3</Col>
+              <Row>
+                {!voteStatus ? (
+                  <Col className="appContent">
+                    <span className="d-flex justify-content-center align-items-center">
+                      <img src={Image.StarIcon} alt="Star Icon" />
+                    </span>
+                    <h1>How did we do? {selectedPoint}</h1>
+                    <p>
+                      Please let us know how we did with your support request.
+                      All feedback is appreciated to help us improve our
+                      offering?
+                    </p>
+                    <Row className="d-flex justify-content-around align-items-center text-center buttonList gx-1">
+                      {points.map((i) => {
+                        return (
+                          <Col key={i}>
+                            <a href="#" onClick={() => setSelectedPoint(i)}>
+                              {i}
+                            </a>
+                          </Col>
+                        );
+                      })}
+                    </Row>
+                    <div className="d-grid gap-2">
+                      <Button
+                        variant="primary"
+                        size="lg"
+                        className="btn-orange btnSubmit"
+                        onClick={() => setVoteStatus(true)}
+                      >
+                        SUBMIT
+                      </Button>
+                    </div>
+                  </Col>
+                ) : (
+                  <Col className="appContent text-center">
+                    <button
+                      type="button"
+                      className="btn-close btn-close-white"
+                      aria-label="Close"
+                      onClick={() => setVoteStatus(false)}
+                    />
+                    <img
+                      src={Image.ThankyouImg}
+                      alt="Thank You"
+                      className="mb-4"
+                    />
+                    <p>
+                      You Selected {selectedPoint} out of {points.length}
+                    </p>
+                    <h1>Thank You!</h1>
+                    <p>
+                      We appreciate youtaking the time to give a rating. If you
+                      ever need more support, don't hesitate to get in touch
+                    </p>
+                  </Col>
+                )}
               </Row>
             </Col>
           </Row>
---
+++ src/modules/images.jsx
@@ -0,0 +1,7 @@
+import StarIcon from "./../assets/svg/icon-star.svg";
+import ThankyouImg from "./../assets/svg/illustration-thank-you.svg";
+
+export default {
+  StarIcon,
+  ThankyouImg,
+};
